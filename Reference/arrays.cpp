084e6e2d4f8d42f928c23408a24b7839ee1d452a
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-03-31 15:38:06 -0700

    Create arrays.cpp

---
+++ Reference/arrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int luckyNums[20] = {4, 8, 15, 16, 23, 42};     //store 20 elements in array
+    cout << luckyNums[0];                           //prints 4, index
+    luckyNums[1] = 19;                              //can modify array as you go
+
+
+    return 0;
+}

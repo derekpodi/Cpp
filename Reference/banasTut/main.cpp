3f20beae44f36059e5a8a64aeb442421640f76ba
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-04-28 08:36:18 -0700

    Update main.cpp
    add Arrays

--- Reference/banasTut/main.cpp
+++ Reference/banasTut/main.cpp
@@ -218,3 +218,31 @@ int main(int argc, char** argv) {
     // Shows bool values as true or false
     cout.setf(ios::boolalpha);
     cout << "Can Derek Vote : " << canIVote << endl;
+    
+    // ---------- ARRAYS ----------
+    // An array is a collection of data of the same type
+    // Declare an array of ints with the value of 1
+    // Once the size is defined it can't change
+    int arrnNums[10] = {1};
+
+    // Leave out the array length
+    int arrnNums2[] = {1,2,3};
+
+    // Create an array and assign some values
+    int arrnNums3[5] = {8, 9};
+
+    // Access array values using indexes starting with 0
+    cout << "1st Value : " << arrnNums3[0] << endl;
+
+    // Change a value
+    arrnNums3[0] = 7;
+    cout << "1st Value : " << arrnNums3[0] << endl;
+
+    // A multidimensional array is like a spreadsheet
+    // If you think of each as layers that contain columns
+    // 1st Number : Which layer
+    // 2nd Number : Which column
+    // 3rd Number : Which row
+    int arrnNums4[2][2][2] = {{{1,2}, {3,4}},
+    {{5,6}, {7,8}}};
+    cout << arrnNums4[0][1][1] << endl;

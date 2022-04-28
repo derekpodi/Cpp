d6b68cfd4a353bf22c117c592e20c49222241f57
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2022-04-28 12:05:10 -0700

    Update main.cpp
    add Functions, Pointers, Exception handling, and Object/Classes.
    Functions below main, also function prototype above main.

--- Reference/banasTut/main.cpp
+++ Reference/banasTut/main.cpp
@@ -430,4 +430,169 @@ int main(int argc, char** argv) {
     // Also sin, cos, tan, asin, acos, atan, atan2,
     // sinh, cosh, tanh, asinh, acosh, atanh
     
-    
+    // ---------- FUNCTIONS ----------
+    // You define function prototypes outside of main
+    // so main knows a functions basic settings
+    // Functions are also created outside of main
+    // and can be found after main below
+
+    // Call a function that sums values
+    printf("%.1f + %.1f = %.1f\n", 5.0,
+            4.0, AddNumbers(5, 4));
+
+    // The function AssignName below creates a
+    // variable called name, but variables created
+    // in functions are not available outside of
+    // that function
+
+    // Variable values changed in a function don't
+    // change that variables value outside of the
+    // function
+    int age2 = 43;
+    AssignAge(age2);
+    cout << "New Age : " << age2 << endl;
+
+    // You could change the value by passing it back
+    age2 = AssignAge2(age2);
+    cout << "New Age " << age2 << "\n";
+
+    // ---------- POINTERS ----------
+    // A Pointer stores a address in memory
+    // You declare a pointer to be of the same type as the data
+    // type to which it points
+    // If you are storing the address of an int then the
+    // pointer is an int
+    // When you declare a pointer give it a null value
+    int* pAge2 = NULL;
+
+    // The reference operator returns the address for a
+    // variable that can be stored in a pointer
+    pAge2 = &age2;
+
+    // Print the address Hexidecimal Number
+    cout << "Address : " << pAge2 << "\n";
+
+    // Get the value at that address with the dereference
+    // operator
+    cout << "Value at Address : " << *pAge2 << "\n";
+
+    // We can create a pointer to an array and cycle
+    // through the data with ++ and --
+    int intArray[] = {1,2,3,4};
+    int* pIntArray = intArray;
+
+    cout << "1st " << *pIntArray <<
+            " Address " << pIntArray << "\n";
+    pIntArray++;
+    cout << "2nd " << *pIntArray <<
+            " Address " << pIntArray << "\n";
+    pIntArray--;
+    cout << "1st " << *pIntArray <<
+            " Address " << pIntArray << "\n";
+
+    // You can pass a pointer to a function and then
+    // the function can change that variables value
+    // globally
+    age2 = 43;
+
+    // Send the address for age to the function
+    AssignAge3(&age2);
+
+    cout << "Pointer Age " << age2 << "\n";
+
+    // Create an array and double values
+    int arr[] = {1,2,3,4};
+    DoubleArray(arr, 4);
+    for(int i = 0; i < 4; ++i){
+        cout << "Array " << arr[i] << "\n";
+    }
+
+    // ---------- EXCEPTION HANDLING ----------
+    // Exceptions are errors that occur when things don't
+    // go as expected
+    // You try to execute code that could be troublesome
+    // and if an error occurs you catch it and throw
+    // it to another block of code for handling
+
+    double num3 = 10, num4 = 0;
+    try{
+        if(num4 == 0){
+            throw "Division by Zero Error";
+        } else {
+            printf("%.1f / %.1f = %.1f", num3, num4,
+                    (num3 / num4));
+        }
+    }
+
+    catch(const char* exp){
+        cout << "Error : " << exp << endl;
+    }
+
+    // ---------- OBJECTS & CLASSES ----------
+    // With object oriented programming we model real
+    // world objects with code. Real objects have
+    // attributes (height, weight) and capabilities
+    // (run, eat)
+    // In object oriented programming we store
+    // attribute values in fields / variables and
+    // we simulate capabilities in methods / functions
+    // A class is a blueprint for modeling the above
+    Shape square(10, 5);
+    Circle circle(10);
+    ShowArea(square);
+    ShowArea(circle);
+
+
+    // The main function returns a 0 when it has executed
+    // without an error
+    return 0;
+}
+
+// ---------- FUNCTIONS ----------
+// A function starts with the data type it will return or void
+// if no value is returned
+// You also list the data types for the parameters passed into
+// a function and you may assign them default values
+
+double AddNumbers(double num1 = 0, double num2 = 0){
+    return num1 + num2;
+}
+
+// A variable created inside a function can't be accessed
+// outside of it
+void AssignName(){
+    string name = "Derek";
+}
+
+void AssignAge(int age){
+    age = 24;
+}
+
+int AssignAge2(int age){
+    age = 24;
+    return age;
+}
+
+// Function that receives a pointer
+void AssignAge3(int* pAge){
+
+    // Use dereference operator to change the value
+    // at the address
+    *pAge = 22;
+
+}
+
+// Double values in array
+void DoubleArray(int *arr, int size){
+    for(int i = 0; i < size; ++i){
+        arr[i] = arr[i] * 2;
+    }
+}
+
+// This function receives Shapes but uses the
+// correct Area() automatically
+// This is an example of polymorphism
+void ShowArea(Shape& shape){
+    cout << "Area : " << shape.Area() << "\n";
+}
+

95c10ecc470972ec812b111429bc9bd3e54d1a98
Derek Podimatis <89597550+derekpodi@users.noreply.github.com>
2023-01-10 20:03:56 -0800

    Add files via upload

--- Reference/while.cpp
+++ Reference/while.cpp
@@ -1,16 +1,23 @@
-// custom countdown using while
-//https://cplusplus.com/doc/tutorial/control/
 #include <iostream>
+
 using namespace std;
 
-int main ()
+
+int main()
 {
-  int n = 10;
+    int index = 1;                      //will not start loop if index=6
+    while(index <= 5){                  //infinite loop if this never becomes true
+        cout << index << endl;
+        index++;
+    }
 
-  while (n>0) {
-    cout << n << ", ";
-    --n;
-  }
+/*
+    do {                                //do-while loop - does code first, then checks condition after
+        cout << index << endl;
+        index++;
+    } while (index <= 5);
+*/
 
-  cout << "liftoff!\n";
+    return 0;
 }
+
